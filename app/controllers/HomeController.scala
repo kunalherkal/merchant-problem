98c073270d3c186d8ea2911b8fb82ae31a601126
Kunal Herkal <kunalrherkal@gmail.com>
2016-03-20 02:53:19 +0530

    Removed AyncController and removed input processing related code from HomeController.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -19,33 +19,9 @@ class HomeController @Inject() extends Controller {
    * a path of `/`.
    */
   def index = Action {
-    info("glob is I")
-    info("prok is V")
-    info("pish is X")
-    info("tegj is L")
 
-    println(RomanNumerals.valueOf("XXIV"))
-    println(RomanNumerals.valueOf("XLII"))
-    println(RomanNumerals.valueOf("MCMXLIV"))
 
-    println(IntergalacticNumerals.valueOf("pish tegj glob glob"))
-
-    Ok(views.html.index("Your new application is ready."))
+    Ok("Your new application is ready.")
   }
-
-  def info(query: String): Int = {
-    val splitQuery = query.split(" ")
-    val romanSymbol = RomanNumerals.getSymbol(splitQuery(2))
-
-    IntergalacticNumerals.addSymbol(splitQuery(0), romanSymbol)
-    IntergalacticNumerals.symbols.foreach(println)
-    0
-  }
-
-  def value(query: String): Int = {
-    0
-  }
-
-
 
 }
